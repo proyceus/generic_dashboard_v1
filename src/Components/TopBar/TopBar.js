888194598f92962a148f0979b28d4930670a184f
Pierre Royce <proyceus@gmail.com>
2021-05-28 15:05:54 -0400

    Finished basic layout of top navbar

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Button from '../UI/Button';
 
 const TopBar = () => {
+  const signupClickHandler = () => {
+    console.log("This is a click");
+  }
   return (
-    <div className="top-bar">
-      <Button text="Sign Up" />
-      <Button text="Log In" />
+    <div className="h-20 block border-2 w-full">
+      <img src="shield_logo.png" alt="Black shield logo" className="h-full w-auto cursor-pointer float-left p-2"></img>
+      <div className="p-4 h-full flex float-right justify-evenly w-60">
+        <Button text="Sign Up" onClick={signupClickHandler} />
+        <Button text="Log In" />
+      </div>
+
     </div>
   )
 }
--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Button = (props) => {
+  const styles = `bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded w-15 ${props.extraStyles}`;
+
   return (
-    <button className="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded">{props.text}</button>
+    <button className={styles} onClick={props.onClick}>{props.text}</button>
   )
 }
 
