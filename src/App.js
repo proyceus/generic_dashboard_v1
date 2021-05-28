78b9bf4b0302d1d537ab4fdd3ecb9a93f0419248
Pierre Royce <proyceus@gmail.com>
2021-05-28 14:11:50 -0400

    Added in Tailwinds CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import TopBar from './Components/TopBar/TopBar'
 
 function App() {
   return (
     <div className="App">
-
+      <TopBar />
     </div>
   );
 }
