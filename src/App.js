55168632fe665691d0cb96e3acc268c9f7652d7d
proyceus <proyceus@gmail.com>
2022-08-30 16:43:22 -0400

    pulled through context and added function to navbar to disappear

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ import {
   Editor,
   Line,
 } from "./pages";
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
