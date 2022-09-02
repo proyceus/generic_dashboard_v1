074a83b4dd6868d3d522ade3e3749d134335c9f7
proyceus <proyceus@gmail.com>
2022-09-02 14:40:43 -0400

    created colorpicker page

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ const App = () => {
             </div>
 
             <div>
+              <ThemeSettings />
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
